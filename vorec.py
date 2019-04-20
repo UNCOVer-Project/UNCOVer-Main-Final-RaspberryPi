e4ecee9c4e7bb2380799542e7645e88c6d307b18
UNCOVer-Project <47940955+UNCOVer-Project@users.noreply.github.com>
2019-04-20 22:39:26 +0700

    Add files via upload

--- vorec.py
+++ vorec.py
@@ -1,8 +1,9 @@
 import os
-# Import playsound as a library to play sound in Raspberry
 from util import playSound
 
-
+# for setting audio purposes
+# putenv is used to modify the environment 
+# using alsa advanced linux sound architecture
 os.putenv('SDL_AUDIODRIVER', 'alsa')
 os.putenv('SDL_AUDIODEV', '/dev/audio')
 playSound('speech.mp3')
