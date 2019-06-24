4fa2994dbc517400a76e7f22c7917f0bf2cbb21f
UNCOVer-Project <47940955+UNCOVer-Project@users.noreply.github.com>
2019-06-24 21:50:01 +0700

    Update vorec.py

--- vorec.py
+++ vorec.py
@@ -1,9 +1,9 @@
 import os
 from util import playSound
 
-# for setting audio purposes
-# putenv is used to modify the environment 
-# using alsa advanced linux sound architecture
+# For setting audio purposes
+# Putenv is used to modify the environment 
+# Using alsa advanced linux sound architecture
 os.putenv('SDL_AUDIODRIVER', 'alsa')
 os.putenv('SDL_AUDIODEV', '/dev/audio')
 playSound('speech.mp3')
